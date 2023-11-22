258608f47a180519a26345d158bc0dede602ddf2
Mohamed Alsaffar <mohammedalsaffar6767@gmail.com>
2023-11-22 19:52:57 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Uighur", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(365, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(372, 63)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Personal finance manager"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Uighur", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 155)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 63)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Income : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Uighur", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(13, 270)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(171, 63)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Expenses : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Uighur", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(14, 377)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(156, 63)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Budgets : "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(198, 174)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(359, 26)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(198, 289)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(359, 26)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(198, 395)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(359, 26)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(450, 488)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 50)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(247, 488)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(160, 50)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Export To Pdf "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 544)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(1103, 345)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(25, 488)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 50)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Show Saved Item"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Uighur", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(38, 900)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 63)
+        Me.Label5.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1139, 1315)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Collections.Specialized.BitVector32
+Imports System.IO
+Imports System.Windows.Documents
+Imports Microsoft.VisualBasic.FileIO
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.Reflection.Metadata
+Imports Paragraph = iTextSharp.text.Paragraph
+Imports System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim filePath As String = "C:\Users\moham\source\repos\finance_manager\File.csv"
+        Dim data As String = TextBox1.Text + "," + TextBox2.Text + "," + TextBox3.Text + "," + DateTime.Now
+        Using writer As StreamWriter = New StreamWriter(filePath, True)
+            writer.WriteLine(data)
+        End Using
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim csvFilePath As String = "C:\Users\moham\source\repos\finance_manager\File.csv"
+        Dim csvData As New DataTable()
+
+        Using parser As New TextFieldParser(csvFilePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row to create columns in the DataTable
+            Dim headers = parser.ReadFields()
+            For Each header In headers
+                csvData.Columns.Add(header)
+            Next
+
+            ' Read the remaining rows and populate the DataTable
+            While Not parser.EndOfData
+                Dim fields = parser.ReadFields()
+                csvData.Rows.Add(fields)
+            End While
+        End Using
+
+        DataGridView1.DataSource = csvData
+
+        Dim pdfFilePath As String = "C:\Users\moham\source\repos\finance_manager\File1.pdf"
+
+        ' Read the CSV file into a DataTable
+        Dim csvData1 As New DataTable()
+        Using parser As New TextFieldParser(csvFilePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row to create columns in the DataTable
+            If Not parser.EndOfData Then
+                Dim headers = parser.ReadFields()
+                For Each header In headers
+                    csvData1.Columns.Add(header)
+                Next
+            End If
+
+            ' Read the remaining rows and populate the DataTable
+            While Not parser.EndOfData
+                Dim fields = parser.ReadFields()
+                csvData1.Rows.Add(fields)
+            End While
+        End Using
+
+        ' Create a PDF document and add the data from the DataTable
+        Using document As New iTextSharp.text.Document()
+            Using writer As PdfWriter = PdfWriter.GetInstance(document, New FileStream(pdfFilePath, FileMode.Create))
+                document.Open()
+
+                ' Add content to the PDF document
+                document.Add(New iTextSharp.text.Paragraph("Hello, this is your finance Report"))
+                document.Add(Chunk.NEWLINE) ' Add an empty line
+
+                ' Create a PDF table and populate it with the data from the DataTable
+                Dim table As New PdfPTable(csvData1.Columns.Count)
+                For Each column As DataColumn In csvData1.Columns
+                    table.AddCell(column.ColumnName)
+                Next
+
+                For Each row As DataRow In csvData1.Rows
+                    For Each cell As Object In row.ItemArray
+                        table.AddCell(cell.ToString())
+                    Next
+                Next
+
+                ' Add the table to the PDF document
+                document.Add(table)
+
+                ' Close the PDF document
+                document.Close()
+            End Using
+        End Using
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.Hide()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim csvFilePath As String = "C:\Users\moham\source\repos\finance_manager\File.csv"
+        Dim csvData As New DataTable()
+
+        Using parser As New TextFieldParser(csvFilePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row to create columns in the DataTable
+            Dim headers = parser.ReadFields()
+            For Each header In headers
+                csvData.Columns.Add(header)
+            Next
+
+            ' Read the remaining rows and populate the DataTable
+            While Not parser.EndOfData
+                Dim fields = parser.ReadFields()
+                csvData.Rows.Add(fields)
+            End While
+        End Using
+
+        DataGridView1.DataSource = csvData
+        ' Calculate the sum for each column
+        Dim sumColumn1 As Double = 0
+        Dim sumColumn2 As Double = 0
+        Dim sumColumn3 As Double = 0
+
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            If Not row.IsNewRow Then
+                Dim value1 As Double
+                Dim value2 As Double
+                Dim value3 As Double
+
+                If Double.TryParse(Convert.ToString(row.Cells(0).Value), value1) Then
+                    sumColumn1 += value1
+                End If
+
+                If Double.TryParse(Convert.ToString(row.Cells(1).Value), value2) Then
+                    sumColumn2 += value2
+                End If
+
+                If Double.TryParse(Convert.ToString(row.Cells(2).Value), value3) Then
+                    sumColumn3 += value3
+                End If
+            End If
+        Next
+
+        ' Display the total outside of the DataGridView
+        Label5.Text = $"Total:
+Total Of Income : {sumColumn1}
+Total Of Expenses : {sumColumn2}
+Total Of Budgets : {sumColumn3}"
+
+        DataGridView1.Show()
+
+    End Sub
+End Class
